215a3b3cd13c50e845e305bc0b870cd1f929500b
Ben Yip <ben_yip@126.com>
2024-03-02 20:34:38 +0800

    collaboration section

---
+++ src/components/Collaboration.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Section from "./Section";
+
+const Collaboration = () => {
+  return (
+    <Section crosses id="collaboration">
+      <div className="container lg:flex">
+        <div className="max-w-[25rem]">
+          <h2 className="h2 mb-4 md:mb-8">
+            AI Chat App for seamless collaboration
+          </h2>
+        </div>
+      </div>
+    </Section>
+  );
+};
+
+export default Collaboration;
