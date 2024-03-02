0b0bf9aa87fd842903848a523dd3701d80d5260c
Ben Yip <ben_yip@126.com>
2024-03-02 17:04:09 +0800

    some additional tag

---
+++ src/components/CompanyLogo.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const CompanyLogo = () => {
+  return <div>CompanyLogo</div>;
+};
+
+export default CompanyLogo;
--- src/components/Generating.tsx
+++ src/components/Generating.tsx
@@ -1,7 +1,21 @@
 import React from "react";
+import { loading } from "../assets";
 
-const Generating = () => {
-  return <div>Generating</div>;
+interface GeneratingProps {
+  className?: string;
+}
+
+const Generating: React.FC<GeneratingProps> = ({ className }) => {
+  return (
+    <div
+      className={`flex items-center h-[3.5rem] px-6 bg-n-8/80 rounded-[1.7rem] ${
+        className || ""
+      } text-base`}
+    >
+      <img src={loading} alt="Loading" className="w-5 h-5 mr-4" />
+      AI is generating
+    </div>
+  );
 };
 
 export default Generating;
